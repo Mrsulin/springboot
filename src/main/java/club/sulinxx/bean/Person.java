6c04ee085c54b6797c903b58a89db99d6c5f3464
Sulin <1848424208@qq.com>
2019-09-18 00:24:43 +0800

    v1.1

--- src/main/java/club/sulinxx/bean/Person.java
+++ src/main/java/club/sulinxx/bean/Person.java
@@ -1,5 +1,6 @@
 package club.sulinxx.bean;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -8,11 +9,36 @@ import java.util.List;
 import java.util.Map;
 
 @Component
-@ConfigurationProperties(prefix = "person")
+//@ConfigurationProperties(prefix = "person")
 public class Person {
+    /**
+     *      <bean class="person">
+     *       <property name="name" value="字面量/${从环境变量、配置文件中获取值}/#{SpEl，也就是spring的表达式语言}"
+     *      </bean>
+     *
+     *
+     *      两种注入方式：(二者不要同时用)
+     *      ①@ConfigurationProperties+@Component和配置文件(properties+yml文件)
+     *      ②单独使用@value注解注入
+     *
+     *      区别：  第一种批量注入                        后者逐个注入
+     *              第一种支持松散绑定(大小写不敏感)      后者大小写铭感               （松散绑定）
+     *              第一种不支持spEL                      后者支持spEL
+     *              第一种支持JSR303数据校验              后者不支持              "@validate修饰类，@Email等修饰字段"
+     *
+     *              重点@value注解在一些复杂类型的封装时候，不支持(例如map)
+     *              如果只是在某个业务逻辑中需要获取一下配置文件中的某项值，使用@value
+     *              如果是在大批量中需要获取配置文件资源使用@ConfigurationProperties
+     **/
+    @Value("${person.name}")
     private String name;
+
+    @Value("#{11*2}")
     private Integer age;
+
+    @Value("true")
     private Boolean boss;
+
     private Date birth;
 
     private Map<String,Object>maps;
